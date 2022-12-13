e32d2cceedf8bb3c63b7ae35fa0deedf20754c3a
N-Upchurch <nathan@komcreative.com>
2022-12-13 17:24:29 -0600

    Fix breakage due to Lyra update

--- assets/js/buildResultCards.js
+++ assets/js/buildResultCards.js
@@ -2,15 +2,16 @@ import { htmlDecode } from './htmlDecode.js';
 
 // Build result cards
 export const buildResultCards = (resultArray, resultsContainer) => {
+	console.log(resultArray);
 	resultArray.forEach(x => {
 		const resultTemplate = document.querySelector('.result');
 		const result = resultTemplate.content.cloneNode(true);
 
 		// Add unique info from searchResult
-		result.querySelector('.resultHeader').innerHTML += `${x.name}`;
-		result.querySelector('sl-avatar').setAttribute("image", `${x.smallHeadshotPath}`);
-		result.querySelector('sl-button').setAttribute("href",`/profiles/${x.profileID}`);
-		result.querySelector('.card-overview').innerHTML += `${htmlDecode(x.bio.slice(0, 450))}...`;
+		result.querySelector('.resultHeader').innerHTML += `${x.document.name}`;
+		result.querySelector('sl-avatar').setAttribute("image", `${x.document.smallHeadshotPath}`);
+		result.querySelector('sl-button').setAttribute("href",`/profiles/${x.document.profileID}`);
+		result.querySelector('.card-overview').innerHTML += `${htmlDecode(x.document.bio.slice(0, 450))}...`;
 		resultsContainer.appendChild(result);
 	});
 }
